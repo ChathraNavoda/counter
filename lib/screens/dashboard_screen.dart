0dd9e073e2d5aa5db2c4195dbbe53fb07fead975
ChathraNavoda <chathra96@gmail.com>
2024-01-09 16:46:25 +0530

    create the navigation between screens

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
@@ -12,7 +13,9 @@ class Dashboard extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           child: const Text("Profile"),
-          onPressed: () {},
+          onPressed: () {
+            GoRouter.of(context).go("/profile");
+          },
         ),
       ),
     );
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
@@ -12,7 +13,9 @@ class Profile extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           child: const Text("Dashborad"),
-          onPressed: (){},
+          onPressed: (){
+             GoRouter.of(context).go("/");
+          },
         ),
       ),
     );
